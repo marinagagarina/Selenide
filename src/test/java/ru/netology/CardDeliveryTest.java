ed94ef80147371118f11aa49232c19f8b7ff067f
Ekaterina Shvedova <arcadia08@mail.ru>
2020-01-26 13:38:11 +0400

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+}
